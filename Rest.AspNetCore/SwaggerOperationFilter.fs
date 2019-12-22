1de0beaac2e35d40f351965976c563e7f8478efa
Julie Koubová <julie@koubova.net>
2019-12-22 09:21:06 +0100

    refactor

--- Rest.AspNetCore/SwaggerOperationFilter.fs
+++ Rest.AspNetCore/SwaggerOperationFilter.fs
@@ -14,54 +14,56 @@ type SwaggerOperationFilter() =
   [<Literal>]
   let ResponseContentType = "application/json"
 
+  let mapSnd f (x, y) =
+    x, f y
+
+  let statusCode (response : IRestResponseDefinition) =
+    response.Status |> StatusCode.code |> int |> string
+
+  let mergeResponses
+    (generateSchema : TypeInfo -> OpenApiSchema)
+    (responses : IRestResponseDefinition list)
+    =
+    let description =
+      responses
+      |> List.map (fun r -> r.Title)
+      |> String.concat "\n"
+
+    let getResponseType (r : IRestResponseDefinition) =
+      match r.IsSuccess with
+      | true -> ResponseContentType, r.ContentType
+      | false -> ProblemContentType, typeof<ProblemDetails>.GetTypeInfo ()
+
+    let mediaType schema =
+      OpenApiMediaType(Schema = schema)
+
+    let result = OpenApiResponse (Description = description)
+
+    responses
+    |> Seq.tryHead
+    |> Option.map (
+      getResponseType
+      >> mapSnd (generateSchema >> mediaType)
+    )
+    |> Option.iter (fun (k, v) ->
+      result.Content.[k] <- v
+    )
+
+    result
 
   interface IOperationFilter with
     member _.Apply (operation, context) =
-      let op = getOperation context.ApiDescription.ActionDescriptor.Properties
-      match op with
+      let generateSchema t =
+        context.SchemaGenerator.GenerateSchema (t, context.SchemaRepository)
+      let props =
+        context.ApiDescription.ActionDescriptor.Properties
+
+      match getOperation props with
       | None -> ()
       | Some op ->
-
-        let mapSnd f (x, y) =
-          x, f y
-
-        let statusCode (response : IRestResponseDefinition) =
-          response.Status |> StatusCode.code |> int |> string
-
-        let mergeResponses (responses : IRestResponseDefinition list) =
-          let description =
-            responses
-            |> List.map (fun r -> r.Title)
-            |> String.concat "\n"
-
-          let getResponseType (r : IRestResponseDefinition) =
-            match r.IsSuccess with
-            | true -> ResponseContentType, r.ContentType
-            | false -> ProblemContentType, typeof<ProblemDetails>.GetTypeInfo ()
-
-          let generateSchema t =
-            context.SchemaGenerator.GenerateSchema (t, context.SchemaRepository)
-
-          let mediaType schema =
-            OpenApiMediaType(Schema = schema)
-
-          let result = OpenApiResponse (Description = description)
-
-          responses
-          |> Seq.tryHead
-          |> Option.map (
-            getResponseType
-            >> mapSnd (generateSchema >> mediaType)
-          )
-          |> Option.iter (fun (k, v) ->
-            result.Content.[k] <- v
-          )
-
-          result
-
         op.Responses
         |> List.groupBy statusCode
-        |> List.map (mapSnd mergeResponses)
+        |> List.map (mapSnd (mergeResponses generateSchema))
         |> List.iter (fun (status, response) ->
             operation.Responses.[status] <- response
         )

13ffdbe2051571bb1d930894c4561c37110c5435
Julie Koubová <julie@koubova.net>
2020-01-15 22:28:44 +0100

    simple queries work!

--- Rest.Tests/QueryEvaluator.fs
+++ Rest.Tests/QueryEvaluator.fs
@@ -15,7 +15,14 @@ let entities = [
 let cases : list<string * float list> =
   [
     ("F eq 1", [1.0])
+    ("not (F ne 1)", [1.0])
     ("F ne 1", [2.0; 10.0])
+    ("F gt 2", [10.0])
+    ("F ge 2", [2.0; 10.0])
+    ("F lt 2", [1.0])
+    ("F le 2", [1.0;2.0])
+    ("(F lt 2) or (F gt 2)", [1.0;10.0])
+    ("(F lt 2) and (F gt 2)", [])
   ]
 
 [<Tests>]
@@ -26,12 +33,13 @@ let tests =
         let result =
           RestExpr.parse<Entity> str |> Result.map (
             (fun expr -> { RestQuery.empty with Filter = Some expr }) >>
-            (fun query -> QueryEvaluator.applyFilter query entities )
+            (fun query -> QueryEvaluator.applyFilter query entities ) >>
+            List.ofSeq
           )
 
         Expect.equal
           result
-          (Result.Ok (expected |> Seq.map (fun f -> { F = f })))
+          (Result.Ok (expected |> List.map (fun f -> { F = f })))
           (sprintf "Unexpected filter result for '%s'" str)
       }
 
--- Rest/QueryEvaluator.fs
+++ Rest/QueryEvaluator.fs
@@ -4,6 +4,16 @@ open System
 open Microsoft.FSharp.Linq.RuntimeHelpers
 open System.Reflection
 
+let private getMethodDef expr =
+  match expr with
+  | Patterns.Call (None, mi,_) -> mi.GetGenericMethodDefinition ()
+  | _ -> failwithf "Expected a generic method call"
+
+let private equality =
+  getMethodDef <@ LanguagePrimitives.GenericEquality 0 0 @>
+let private comparison =
+  getMethodDef <@ LanguagePrimitives.GenericComparison 0 0 @>
+
 let applyFilter<'T> ({ Filter = filter } : RestQuery<'T>) (seq : 'T seq) =
   match filter with
   | None -> seq
@@ -23,14 +33,21 @@ let applyFilter<'T> ({ Filter = filter } : RestQuery<'T>) (seq : 'T seq) =
       let convert = <@@ System.Convert.ChangeType (%%expr, target) @@>
       Expr.Coerce (convert, target)
 
-    let compileBinary op left right =
+    let compileBinary op (left : Expr) (right : Expr) =
+
+      let equals =
+        Expr.Call ((equality.MakeGenericMethod ([|left.Type|])), [left; right])
+
+      let compare =
+        Expr.Call ((comparison.MakeGenericMethod ([| left.Type|])), [left;right])
+
       match op with
-      | ExprAst.Equal -> <@@ %%left = %%right @@>
-      | ExprAst.NotEqual -> <@@ %%left <> %%right @@>
-      | ExprAst.LessThan -> <@@ %%left < %%right @@>
-      | ExprAst.LessThanOrEqual -> <@@ %%left <= %%right @@>
-      | ExprAst.GreaterThan -> <@@ %%left > %%right @@>
-      | ExprAst.GreaterThanOrEqual -> <@@ %%left >= %%right @@>
+      | ExprAst.Equal -> equals
+      | ExprAst.NotEqual -> <@@ not %%equals @@>
+      | ExprAst.LessThan -> <@@ %%compare < 0 @@>
+      | ExprAst.LessThanOrEqual -> <@@ %%compare <= 0 @@>
+      | ExprAst.GreaterThan -> <@@ %%compare > 0 @@>
+      | ExprAst.GreaterThanOrEqual -> <@@ %%compare >= 0 @@>
       | ExprAst.And -> <@@ %%left && %%right @@>
       | ExprAst.Or -> <@@ %%left || %%right @@>
 
@@ -49,13 +66,12 @@ let applyFilter<'T> ({ Filter = filter } : RestQuery<'T>) (seq : 'T seq) =
         id
 
     let lambda =
-      Expr.Lambda (var, body)
+      Expr.NewDelegate (typeof<Func<'T, bool>>, [var], body)
 
     let linq =
-      lambda
-      :?> Expr<'T -> bool>
+      Expr.Cast<Func<'T, bool>> lambda
       |> LeafExpressionConverter.QuotationToLambdaExpression
 
     let compiled = linq.Compile ()
 
-    seq |> Seq.filter compiled
+    seq |> Seq.filter compiled.Invoke

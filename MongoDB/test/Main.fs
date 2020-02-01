34a4a86cec57085d3ad15d289e0fd5adaa7301bf
Julie Koubová <julie@koubova.net>
2020-02-01 19:31:21 +0100

    nowarn

--- MongoDB/test/Main.fs
+++ MongoDB/test/Main.fs
@@ -1,9 +1,7 @@
 ﻿module Rest.MongoDB.Tests.Main
 open Expecto
 
-#nowarn "46" // The identifier 'parallel' is reserved for future use by F#
-
 [<EntryPoint>]
 let main argv =
-  let cfg = { defaultConfig with parallel = false }
+  let cfg = { defaultConfig with ``parallel`` = false }
   runTestsInAssembly cfg argv

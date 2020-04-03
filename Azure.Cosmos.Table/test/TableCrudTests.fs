41c7760ce19ab309373b2b9cc06e7caae881c7ff
Julie Koubová <julie@koubova.net>
2020-04-03 12:03:12 +0200

    cosmos table

---
+++ Azure.Cosmos.Table/test/TableCrudTests.fs
@@ -0,0 +1,42 @@
+module Restfu.Azure.Cosmos.Table.TableTests
+
+open MongoDB.Driver
+open Rest
+open Rest.MongoDB
+open Rest.Tests
+open System
+open System.Threading
+open Expecto
+
+// let mongoUrl =
+//   Environment.GetEnvironmentVariable("MONGODB_URL")
+//   |> NonEmptyString.create
+//   |> Option.orElse (NonEmptyString.create("mongodb://localhost:27017/"))
+//   |> Option.get
+
+// let db =
+//   MongoClient(mongoUrl.Value).GetDatabase("restfu-test")
+
+// let mutable collectionIndex = 0
+
+// let withMongoDBResource f () =
+//   let collectionName =
+//     sprintf "thingies-%i" (Interlocked.Increment (&collectionIndex))
+
+//   let collection = db.GetCollection<Thingy>(collectionName)
+//   collection.DeleteMany(fun _ -> true) |> ignore
+
+//   let res =
+//     MongoDBResource.Create(collection, fun e -> e.Id)
+//     |> RestApiExplorer.applyEntityKeyName
+
+//   try
+//     f res
+//   finally
+//     db.DropCollection(collectionName)
+
+[<Tests>]
+let tests =
+  testList "Azure.Cosmos.Table" [
+    // yield! testFixture withMongoDBResource CrudTests.tests
+  ]
